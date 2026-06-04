a06ad1f2a113f7cf23ee617bc245ecd96b50c895
magicami <magicami86@gmail.com>
2026-06-04 15:58:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const ENABLE_THINKING_MODE = false; // Set to true to enable chat_template_kwarg
 //deepseek-ai/deepseek-v4-pro
 //deepseek-ai/deepseek-v4-flash
 const MODEL_MAPPING = {
-  'gpt-3.5-turbo': 'deepseek-ai/deepseek-v4-pro',
+  'gpt-3.5-turbo': 'deepseek-ai/deepseek-v4-flash',
   'gpt-4': 'qwen/qwen3-coder-480b-a35b-instruct',
   'gpt-4-turbo': 'moonshotai/kimi-k2-instruct-0905',
   'gpt-4o': 'deepseek-ai/deepseek-v3.1',
@@ -88,7 +88,7 @@ app.post('/v1/chat/completions', async (req, res) => {
         } else if (modelLower.includes('claude') || modelLower.includes('gemini') || modelLower.includes('70b')) {
           nimModel = 'meta/llama-3.1-70b-instruct';
         } else {
-          nimModel = 'deepseek-ai/deepseek-v4-pro';
+          nimModel = 'deepseek-ai/deepseek-v4-flash';
         }
       }
     }

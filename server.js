87d15c770efa7f2abe3787079787440ea5373a81
magicami <magicami86@gmail.com>
2026-07-07 14:37:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const ENABLE_THINKING_MODE = false; // Set to true to enable chat_template_kwarg
 //nvidia/nemotron-3-super-120b-a12b //very good, doesn't work for non con
 //nvidia/nemotron-3-ultra-550b-a55b
 const MODEL_MAPPING = {
-  'gpt-3.5-turbo': 'nvidia/nemotron-3-super-120b-a12b',
+  'gpt-3.5-turbo': 'deepseek-ai/deepseek-v4-flash',
   'gpt-4': 'qwen/qwen3-coder-480b-a35b-instruct',
   'gpt-4-turbo': 'moonshotai/kimi-k2-instruct-0905',
   'gpt-4o': 'deepseek-ai/deepseek-v3.1',
@@ -90,7 +90,7 @@ app.post('/v1/chat/completions', async (req, res) => {
         } else if (modelLower.includes('claude') || modelLower.includes('gemini') || modelLower.includes('70b')) {
           nimModel = 'meta/llama-3.1-70b-instruct';
         } else {
-          nimModel = 'nvidia/nemotron-3-super-120b-a12b';
+          nimModel = 'deepseek-ai/deepseek-v4-flash';
         }
       }
     }
